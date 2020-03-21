a04fc791aa2c89b88e97d8921009acba4fbebbec
Oleksandr Holubenko <naturlichgood@gmail.com>
2020-03-21 19:03:53 +0200

    Updates

---
+++ precache-manifest.7d25a5b24b3176b32b026f651da9a6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3bbe0383ff0a5d517b16460b844af9",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "e7cb324f74df602b16b0",
+    "url": "/Todo-App/static/css/main.50ae1115.chunk.css"
+  },
+  {
+    "revision": "2a26642005826e881130",
+    "url": "/Todo-App/static/js/2.2005fa3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.2005fa3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7cb324f74df602b16b0",
+    "url": "/Todo-App/static/js/main.ae6164fa.chunk.js"
+  },
+  {
+    "revision": "bc6143ed291b9404aad4",
+    "url": "/Todo-App/static/js/runtime-main.e9c94070.js"
+  }
+]);
